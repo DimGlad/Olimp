5460762b829f5bfb91f09942241e7751ec83947e
DimGlad <105922220+DimGlad@users.noreply.github.com>
2022-05-20 16:50:19 +0500

    Add files via upload

---
+++ олимп/2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 15);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(503, 327);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество участников:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество судей:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 348);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(217, 383);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(415, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(19, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "(Введите значение от 3 до 10)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 424);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Чемпионат по фигурному катанию";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ олимп/2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)//Доступ к кнопке
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+
+
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)//Ограничение ввода (только цифры)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+                if (Int32.Parse(textBox2.Text) > 10 || Int32.Parse(textBox2.Text) < 3)//Условие о количестве судей
+                {
+                    MessageBox.Show("Судей не может быть меньше 3 и больше 10");//Вывод ошибки при неправильном заполнении количества судей
+                }
+                else
+                {
+                    int part = Int32.Parse(textBox1.Text);//Кол-во участников
+                    int judges = Int32.Parse(textBox2.Text);//Кол-во судей
+                    double[,] Marks = new double[part, judges];//Массив оценок
+
+                    int rows = part + 1;//Строки
+                    int columns = judges + 2;//Столбцы
+                    dataGridView1.RowCount = rows;//Указываем кол-во строк в таблице
+                    dataGridView1.ColumnCount = columns;//Указываем кол-во столбцов в таблице
+                    
+                    dataGridView1.Rows[0].Cells[columns - 1].Value = "Итог";//Название последнего столбца
+                    for (int i = 1; i < rows; i++)//Заполнение заголовков
+                    {
+                        dataGridView1.Rows[i].Cells[0].Value = $"Фигурист {i}";
+                    }
+                    for (int i = 1; i < columns - 1; i++)//Заполнение заголовков
+                    {
+                        dataGridView1.Rows[0].Cells[i].Value = $"Судья {i}";
+                    }
+                    Random rand = new Random();
+                    for (int i = 0; i < part; i++)//Цикл заполнения массива рандомом
+                    {
+                        for (int j = 0; j < judges; j++)
+                        {
+                            Marks[i, j] = Convert.ToDouble(rand.Next(100) / 10.0 + 0.1);
+                        }
+                    }
+
+                    for (int i = 1; i < rows; i++)//Заполнение таблицы элементами массива оценок
+                    {
+                        for (int j = 1; j < columns - 1; j++)
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = Marks[i - 1, j - 1];
+                        }
+                    }
+
+                    double[] summ = new double[part];//Массив оценок
+                    for (int i = 0; i < part; i++)
+                    {
+                        double sum = 0;//Сумма 
+                        double max = 0;//Максимум
+                        double min = 11;//Минимум
+                        for (int j = 0; j < judges; j++)
+                        {
+                            sum += Marks[i, j];
+                            if (max < Marks[i, j])//Находим максимум строкм
+                            {
+                                max = Marks[i, j];
+                            }
+                            if (min > Marks[i, j])//Находим минимум строки
+                            {
+                                min = Marks[i, j];
+                            }
+
+                        }
+                        summ[i] = (sum - max - min) / (Convert.ToDouble(judges) - 2);//Нахождение итоговых оценок
+                                                                                    
+                    }
+
+                    for (int i = 1; i < rows; i++)//Цикл вывода итоговых оценок
+                    {
+                        dataGridView1.Rows[i].Cells[columns - 1].Value = Math.Round(summ[i - 1], 1);
+                    }
+
+                    
+                }
+            
+           
+        }
+    }
+}
